1128a470206f91abaa795a3f89c49de459987ca3
dlenh <dlenh425@gmail.com>
2022-02-11 11:05:04 -0800

    1. Added html and js files 2. Added computerPlay function

---
+++ script.js
@@ -0,0 +1,13 @@
+function computerPlay() {
+    const arr = ["Rock", "Paper", "Scissors"];
+    let random = Math.floor(Math.random()*3);
+    return arr[random];
+}
+
+
+// function playRound(playerSelection, computerSelection) {
+
+// }
+
+// const playerSelection = "rock";
+// const computerSelection = computerPlay();
